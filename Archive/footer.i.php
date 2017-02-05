14bccacef64bd0b873cf777b0f91d71203cb1edf
Barton Phillips <bartonphillips@gmail.com>
2017-02-05 12:41:50 -0800

    added Archive with the old look

---
+++ Archive/footer.i.php
@@ -0,0 +1,43 @@
+<?php
+
+$lastmod = date("M j, Y H:i", getlastmod());
+
+return <<<EOF
+<footer>
+Return to Applitec <a class='underline' href="/">Home Page</a>
+{$arg['msg']}
+
+{$arg['msg1']}
+<h2 class='center'><a href="aboutwebsite.php">About This Site</a></h2>
+<address>
+  Copyright &copy; $this->copyright<br>
+  23542 Lyons Ave. #209<br>
+  Newhall, CA 91321<br>
+  <a href="mailto:barton@applitec.com?SUBJECT=WebMaster+feedback">webmaster@applitec.com</a><br>
+  Phone: (818)652-9849<br>
+</address>
+<hr>
+{$arg['msg2']}
+$counterWigget
+<p class="center">Last Modified: $lastmod</p>
+</footer>
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+var sc_project=10235592; 
+var sc_invisible=1; 
+var sc_security="ecfab7a7"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="free hit
+counter" href="http://statcounter.com/free-hit-counter/"
+target="_blank"><img class="statcounter"
+src="http://c.statcounter.com/10235592/0/ecfab7a7/1/"
+alt="free hit counter"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
+</body>
+</html>
+EOF;
---
+++ Archive/head.i.php
@@ -0,0 +1,35 @@
+<?php
+return <<<EOF
+<!DOCTYPE html> 
+<html>
+<head>
+  <title>{$arg['title']}</title>
+  <meta name=viewport content="width=device-width, initial-scale=1">
+  <meta charset='utf-8'>
+  <meta name="copyright" content="$this->copyright">
+  <meta name="Author" content="$this->author">
+  <meta name="Description" content="{$arg['desc']}">
+  <meta name="KeyWords" content="Engineering, Electrical Engineering, Programming, Firmware, Software, Test Equipment,
+        Computers, SCSI Software, SCSI Firmware, FPLD, FPGA, EPLD, CPLD, Windows, UNIX, Linux, Consulting,
+        Analysis, Controllers, ISDN, Network, ATM, Internet, 
+        USB, C, C++">
+  <link rev="MADE" href="mailto:barton@applitec.com"> 
+  <!-- Link our custom CSS -->
+  <link rel="stylesheet" href="http://bartonphillips.net/css/nav.css">
+  <link rel="stylesheet" title="Applitec Style Sheet" href="http://bartonphillips.net/css/applitec.css">
+{$arg['link']}
+  <!-- jQuery -->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+  <script>
+var lastId = $this->LAST_ID;
+jQuery(document).ready(function($) {
+  $("#logo").attr('src', "/tracker.php?page=script&id=$this->LAST_ID");
+});
+  </script>
+  <script async src="http://bartonphillips.net/js/tracker.js"></script>
+{$arg['extra']}
+{$arg['script']}
+{$arg['css']}
+</head>
+<body>
+EOF;
